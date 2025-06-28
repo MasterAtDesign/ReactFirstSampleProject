409a14150f1736b919be176f34af42658ead8df7
MasterAtDesign <roselindeepa87@gmail.com>
2025-06-28 11:52:32 +0530

    creating first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+  // remive strict mode to avoid double rendering in development
+ 
     <App />
-  </StrictMode>,
+ 
 )
